bcb7d6130431a30149c813a47212efad04ca8f08
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:49:25 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,14 +1,14 @@
 #lang racket
 
-; (exists? pred lst): F X L -> B
+; (list.any pred lst): F X L -> B
 ; returns #t if any element of lst satisfies pred, #f otherwise
 ; : size(lst)=0      -> #f
 ; : (pred lst[0])=#t -> #t
-; : else             -> (exists? pred lst[1..end])
-(define exists?
+; : else             -> (list.any pred lst[1..end])
+(define list.any
   (lambda (pred lst)
     (if (null? lst)
         #f
         (if (pred (car lst))
             #t
-            (exists? pred (cdr lst))))))
+            (list.any pred (cdr lst))))))
